e6c9a2301ec76b7b5debeb1d707713e8a7e40757
nonam3e <lendedland@gmail.com>
2022-05-12 20:20:43 +0700

    tree drawing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
@@ -39,6 +41,10 @@ public:
 		m_root = new tree_elem(key);
 		m_size = 1;
 	}
+    explicit binary_tree () {
+        m_root = nullptr;
+        m_size = 0;
+    }
 	binary_tree (const binary_tree& other) {
 		stack <tree_elem *> node_stack;
 		m_root = new tree_elem(*other.m_root);
@@ -58,14 +64,14 @@ public:
 	binary_tree& operator = (const binary_tree& other) {
 		if(this == &other) return *this;
 		delete_tree(m_root);
-		stack <tree_elem *> node_stack;
+		queue <tree_elem *> node_stack;
 		m_root = new tree_elem(*other.m_root);
 		m_root->m_right = nullptr;
 		m_root->m_left = nullptr;
 		m_size = 1;
 		node_stack.push(other.m_root);
 		while (!node_stack.empty()) {
-			tree_elem *temp = new tree_elem(*node_stack.top());
+			tree_elem *temp = new tree_elem(*node_stack.front());
 			this->insert(temp->m_data);
 			node_stack.pop();
 			if (temp->m_right) node_stack.push(temp->m_right);
@@ -79,7 +85,9 @@ public:
 		delete_tree (m_root);
 	}
 	void print () {
-		print_tree (m_root);
+        if (m_root != nullptr)
+		    print_tree (m_root);
+        else cout<<"empty";
 		cout << endl;
 	}
 	bool find (int key) {
@@ -95,22 +103,28 @@ public:
 		return false;
 	}
 	void insert (int key) {
-		tree_elem * curr = m_root;
-		while (curr && curr->m_data != key) {
-			if (curr->m_data > key && curr->m_left == nullptr) {
-				curr->m_left = new tree_elem(key);
-				++m_size;
-				return;
-			}
-			if (curr->m_data < key && curr->m_right == nullptr) {
-				curr->m_right = new tree_elem(key);
-				++m_size;
-				return;
-			}
-			if (curr->m_data > key)
-				curr = curr->m_left;
-			else
-				curr = curr->m_right;
+        if (m_root == nullptr) {
+            m_root = new tree_elem(key);
+            ++m_size;
+        }
+        else {
+            tree_elem *curr = m_root;
+            while (curr && curr->m_data != key) {
+                if (curr->m_data > key && curr->m_left == nullptr) {
+                    curr->m_left = new tree_elem(key);
+                    ++m_size;
+                    return;
+                }
+                if (curr->m_data < key && curr->m_right == nullptr) {
+                    curr->m_right = new tree_elem(key);
+                    ++m_size;
+                    return;
+                }
+                if (curr->m_data > key)
+                    curr = curr->m_left;
+                else
+                    curr = curr->m_right;
+            }
 		}
 	}
 	int min() {
@@ -147,8 +161,13 @@ public:
 				newCurr = curr->m_right;
 			else
 				newCurr = curr->m_left;
-			if (prev == nullptr)
-				return;
+			if (prev == nullptr) {
+                prev = m_root;
+                m_root = newCurr;
+                delete prev;
+                m_size--;
+                return;
+            }
 			if (curr == prev->m_left)
 				prev->m_left = newCurr;
 			else
@@ -171,9 +190,52 @@ public:
 			delete temp;
 		}
 		m_size--;
-		return;
 	}
-	int size() {
+    int height() {
+        if (m_root == nullptr) return 0;
+        int max = 0;
+        int cur_height;
+        stack <pair<tree_elem *, int>> node_stack;
+        node_stack.push(make_pair(m_root, 0));
+        while (!node_stack.empty()) {
+            auto *temp = new tree_elem(*node_stack.top().first);
+            cur_height = node_stack.top().second;
+            node_stack.pop();
+            if (temp->m_right) node_stack.push(make_pair(temp->m_right,cur_height+1));
+            if (temp->m_left) node_stack.push(make_pair(temp->m_left,cur_height+1));
+            delete temp;
+            if (cur_height > max) max = cur_height;
+        }
+        return max;
+    }
+
+    friend ostream& operator << (ostream& out, binary_tree &self) {
+        if (self.m_root == nullptr) {
+            out<<"empty"<<endl;
+            return out;
+        }
+        int space = 0;
+        tree_elem *cur = self.m_root;
+        stack <pair<tree_elem*, int>> s;
+        while (cur != nullptr || !s.empty()) {
+            while (cur != nullptr) {
+                s.push(make_pair(cur, space += 5));
+                cur = cur->m_right;
+            }
+            cur = s.top().first;
+            space = s.top().second;
+            for (int i = 5; i < space; ++i) {
+                out << " ";
+            }
+            out << cur->m_data << endl;
+            s.pop();
+            cur = cur->m_left;
+
+        }
+        return out;
+    }
+    
+	int size() const {
 		return m_size;
 	}
 };
@@ -185,13 +247,20 @@ int main()
 	binary_tree tree (13);
 	for (int i : s) { tree.insert(i); }
 	if (tree.find(2) && tree.min() == 2 && tree.max() == 19 && tree.size() == 10)
-		tree.print();
+        cout << "------------------------\nheight: " << tree.height() << endl << "------------------------" << endl
+             << tree;
 	binary_tree tree_check(3);
 	tree_check = tree;
-	cout<<tree_check.size()<<endl;
-	tree_check.print();
+	cout<<"------------------------\nsize: "<<tree_check.size()<<endl <<"------------------------"<<endl;
+	cout<<tree_check;
 	binary_tree tree_check_second(tree_check);
-	tree.erase(13);
+    tree.print();
+    for (int i : s) { tree.erase(i); }
+    tree.erase(13);
+    tree.print();
+    cout<<tree.size()<<endl;
+    tree.insert(9);
+    tree.erase(13);
 	tree.print();
 	cout<<tree.size()<<endl;
 
